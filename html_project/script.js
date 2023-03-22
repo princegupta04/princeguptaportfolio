74e10db417e7d2d287a5449aa3ef130af14fd20f
PRINCE GUPTA <98451406+princegupta04@users.noreply.github.com>
2023-03-22 23:59:24 +0530

    Add files via upload

---
+++ html_project/script.js
@@ -0,0 +1,21 @@
+console.log("script is running");
+document.querySelector('.cross').style.display =' none';
+
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+   
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+
+    if(document.querySelector('.sidebar').classList.contains('sidebargo'))
+{
+    document.querySelector('.cross').style.display ="none";
+    document.querySelector('.ham').style.display ="inline";
+}
+else{
+    document.querySelector('.ham').style.display ="none";
+    setTimeout(()=>{
+        document.querySelector('.cross').style.display ="inline";
+    },500);
+   
+}
+})
+
